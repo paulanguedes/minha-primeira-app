7a958ac15a52ad9bf17836ab777fbc9649c08725
paulanguedes <paulanguedes@gmail.com>
2023-10-22 20:23:08 -0300

    :monocle_face: criando Model Aluno

---
+++ src/MinhaAppFuncional/Models/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAppFuncional.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto")]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        //[RegularExpression(@"/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i", ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Confirmação de email")]
+        [Compare("Email", ErrorMessage = "Os emails informados não são iguais")]
+        public string EmailConfirmacao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve estar entre {1} e {2}")]
+        public int Avaliacao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
